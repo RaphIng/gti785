40c2533bf862560e1b4b16a2ca1363fd5b834a47
RaphIng <stunsoirousahang@gmail.com>
2016-05-17 20:08:19 -0400

    essaie du media player

--- app/src/main/java/com/example/ak92790/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/ak92790/musicplayer/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.ak92790.musicplayer;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +18,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            String _path0 =  Environment.getExternalStorageDirectory().toString()+"/Music/Kalimba.mp3";
+            File f = new File(_path0);
+
+            FileInputStream fileInputStream = new FileInputStream(f);
+
+            //FileDescriptor fd = fileInputStream.getFD();
+
+
+
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            //mediaPlayer.setDataSource(fd);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+        }
+        catch(Exception e)
+        {
+            System.out.println("echec");
+        }
+
     }
 
 }
